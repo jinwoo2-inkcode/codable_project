0726ff69feda771bc80c81d7244fbcf5f986d549
jasonkim0808 <44018744+jasonkim0808@users.noreply.github.com>
2021-03-14 18:12:17 -0500

    added commits

--- codable/project/Structure.py
+++ codable/project/Structure.py
@@ -4,12 +4,31 @@ create objects and each function for it
 import vpython as vp
 
 #atoms
-def atoms(atomName, c, size, loc):
+def atoms(atomName: string, c: color, size: float, loc: vector) -> atom, vector:
+    """
+    Creates sphere objects for atoms
+    args:
+        atomName: name of atom
+        c: color of sphere object
+        size: radius of sphere object
+        loc: vector of location of sphere object
+    returns:
+        Creates sphere object within 3D space
+        Returns vector location of sphere object
+    """
     atom = vp.sphere(pos=loc, radius=size, color = c)
     location = loc
     return atom, location
     
 #chains
-def chains(atom1, atom2):
+def chains(atom1: vector, atom2: vector) -> cylinder:
+    """
+    Creates cylinder objects for chains
+    args:
+        atom1: vector location of first atom
+        atom2: vector location of second atom
+    returns:
+        Creates cylinder object within 3D space
+    """
     chain = vp.cylinder(pos = atom1, axis = atom2-atom1, radius=0.05)
     return chain
--- codable/project/searchAtom.py
+++ codable/project/searchAtom.py
@@ -5,6 +5,14 @@ Also takes periotic table api
 
 import periodictable as pt
 
-def getAtom(atomNumber):
+def getAtom(atomNumber: int) -> string, float:
+    """
+    Find data for element with atomic number
+    args:
+        atomNumber: Atomic number of wanted element
+    returns:
+        Returns name of element
+        Returns mass of element
+    """
     elem = elements[atomNumber]
     return elem.name, elem.mass
