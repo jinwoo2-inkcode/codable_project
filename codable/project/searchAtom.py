d1a9496f2fbf1a8ee2066323b0a100556e79a6ea
jasonkim0808 <44018744+jasonkim0808@users.noreply.github.com>
2021-03-14 16:27:17 -0500

    testcase

--- codable/project/searchAtom.py
+++ codable/project/searchAtom.py
@@ -2,3 +2,9 @@
 used for search algorithm of elements in periodic table
 Also takes periotic table api
 """
+
+import periodictable as pt
+
+def getAtom(atomNumber):
+    elem = elements[atomNumber]
+    return elem.name, elem.mass
