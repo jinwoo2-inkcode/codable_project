475727845c5abe2ae4cff5c6406ba4f4f93210a9
jasonkim0808 <44018744+jasonkim0808@users.noreply.github.com>
2021-03-14 16:22:16 -0500

    added periodic table package instead of api, made simple objects of sphere and rod

--- codable/project/Structure.py
+++ codable/project/Structure.py
@@ -1,15 +1,15 @@
 """
 create objects and each function for it
 """
+import vpython as vp
 
 #atoms
-def atoms(atom, color, size):
-    #return atom
+def atoms(atomName, c, size, loc):
+    atom = vp.sphere(pos=loc, radius=size, color = c)
+    location = loc
+    return atom, location
     
 #chains
-def chain(atom1, atom2, length):
-    #return chain object
-
-def compound(chainedAtoms):
-    #return compound
-
+def chains(atom1, atom2):
+    chain = vp.cylinder(pos = atom1, axis = atom2-atom1, radius=0.05)
+    return chain
