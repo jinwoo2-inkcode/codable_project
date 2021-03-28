6a01c3c8e5c83d076a6aba222d2a12d629363edb
jasonkim0808 <44018744+jasonkim0808@users.noreply.github.com>
2021-03-28 16:38:31 -0500

    added text of api data but unsuuccessful in vector location

--- codable/project/testcase.py
+++ codable/project/testcase.py
@@ -2,12 +2,19 @@ import Structure as sct
 import searchAtom as sa
 import vpython as vp
 
-hydrogen, h_loc = sct.atoms("H", vp.color.red, 1, vp.vector(0,0,0))
-oxygen1, o1_loc = sct.atoms("O", vp.color.blue, 0.5, vp.vector(2,2,2))
-oxygen2, o2_loc = sct.atoms("O", vp.color.blue, 0.5, vp.vector(-2,2,2))
+oxygen, o_loc = sct.atoms("H", vp.color.red, 1, vp.vector(0,0,0))
+hydrogen1, h1_loc = sct.atoms("O", vp.color.blue, 0.5, vp.vector(2,2,2))
+hydrogen2, h2_loc = sct.atoms("O", vp.color.blue, 0.5, vp.vector(-2,2,2))
 
-h2o = sct.chains(h_loc, o1_loc)
-h2o = sct.chains(h_loc, o2_loc)
+h2o = sct.chains(o_loc, h1_loc)
+h2o = sct.chains(o_loc, h2_loc)
 sct.dataDisplay('test', vp.color.white, vp.vector(4,4,4))
 
-print(sa.getAtom(12))
+name = sa.getAtom(1)[0]
+mass = sa.getAtom(1)[1]
+
+# print(name)
+# print(mass)
+
+# sct.dataDisplay(name, vp.color.white, vp.vector(4,4,4))
+# sct.dataDisplay(str(mass), vp.color.yellow, vp.vector(0,0,0))
