bf577ea53667e62b56e17a285449ff5a80e04909
jasonkim0808 <44018744+jasonkim0808@users.noreply.github.com>
2021-04-04 17:38:13 -0500

    .

--- codable/project/searchCompound.py
+++ codable/project/searchCompound.py
@@ -1,6 +1,12 @@
 """
 used for search algorithm of compound in api
 """
-from chemspipy import chemspipy
+from chemspipy import ChemSpider
 
-cs = ChemSpider('api')
+cs = ChemSpider('J3XTYvy3MAuYai74BsJBm6krPMOWmtLY')
+
+c = cs.get_compound(2157)
+
+print(c.molecular_formula)
+print(c.smiles)
+print(c.common_name)
