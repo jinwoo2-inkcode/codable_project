c3607612fc61bcccabce7d87f70b336d223ac015
jasonkim0808 <44018744+jasonkim0808@users.noreply.github.com>
2021-04-25 15:54:17 -0500

    gui within vpython

--- codable/project/searchCompound.py
+++ codable/project/searchCompound.py
@@ -1,26 +1,30 @@
 """
 used for search algorithm of compound in api
 """
-# from chemspipy import ChemSpider
+from chemspipy import ChemSpider
 
-# cs = ChemSpider('J3XTYvy3MAuYai74BsJBm6krPMOWmtLY')
+cs = ChemSpider('J3XTYvy3MAuYai74BsJBm6krPMOWmtLY')
 
-# def getInfo(compound_number):
-#     c = cs.get_compound(compound_number)
-#     return c.molecular_formula + '\n' + c.smiles + '\n' + c.common_name
+def getInfo(compound_number):
+    c = cs.get_compound(compound_number)
+    return c.molecular_formula + '\n' + c.smiles + '\n' + c.common_name
 
-# # def getIDNumber():
-# #     return
-
-
-
-# print(getInfo(2157))
-
-import pymatgen.core as mg
+# def getIDNumber():
+#     return
 
 
-si = mg.Element("Si")
-comp = mg.Composition("O3Fe2")
 
-print(comp)
-#print(si.atomic_mass)
+print(getInfo(2157))
+
+# import pymatgen.core as mg
+
+
+# si = mg.Element("Si")
+# comp = mg.Composition("O3Fe2")
+
+# lattice = mg.Lattice.cubic(4.2)
+# structure = mg.Structure(lattice, ["Cs", "Cl"],
+#                          [[0, 0, 0], [0.5, 0.5, 0.5]])
+
+# print(comp)
+# #print(si.atomic_mass)
