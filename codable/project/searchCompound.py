745698d2ba2c58d1d5215a3db4e983932d445109
jasonkim0808 <44018744+jasonkim0808@users.noreply.github.com>
2021-04-25 14:46:37 -0500

    playing around with new package, made base for dfs

--- codable/project/searchCompound.py
+++ codable/project/searchCompound.py
@@ -1,17 +1,26 @@
 """
 used for search algorithm of compound in api
 """
-from chemspipy import ChemSpider
+# from chemspipy import ChemSpider
 
-cs = ChemSpider('J3XTYvy3MAuYai74BsJBm6krPMOWmtLY')
+# cs = ChemSpider('J3XTYvy3MAuYai74BsJBm6krPMOWmtLY')
 
-def getInfo(compound_number):
-    c = cs.get_compound(compound_number)
-    return c.molecular_formula + '\n' + c.smiles + '\n' + c.common_name
+# def getInfo(compound_number):
+#     c = cs.get_compound(compound_number)
+#     return c.molecular_formula + '\n' + c.smiles + '\n' + c.common_name
+
+# # def getIDNumber():
+# #     return
 
 
-print(getInfo(2157))
 
-# print(c.molecular_formula)
-# print(c.smiles)
-# print(c.common_name)
+# print(getInfo(2157))
+
+import pymatgen.core as mg
+
+
+si = mg.Element("Si")
+comp = mg.Composition("O3Fe2")
+
+print(comp)
+#print(si.atomic_mass)
--- codable/project/testcase.py
+++ codable/project/testcase.py
@@ -4,9 +4,9 @@ import vpython as vp
 import searchCompound as sc
 
 #create water
-oxygen, o_loc = sct.atoms("H", vp.color.red, 1, vp.vector(0,0,0))
-hydrogen1, h1_loc = sct.atoms("O", vp.color.blue, 0.5, vp.vector(2,2,2))
-hydrogen2, h2_loc = sct.atoms("O", vp.color.blue, 0.5, vp.vector(-2,2,2))
+oxygen, o_loc = sct.atoms("O", vp.color.red, 1, vp.vector(0,0,0))
+hydrogen1, h1_loc = sct.atoms("H", vp.color.blue, 0.5, vp.vector(2,2,2))
+hydrogen2, h2_loc = sct.atoms("H", vp.color.blue, 0.5, vp.vector(-2,2,2))
 
 h2o = sct.chains(o_loc, h1_loc)
 h2o = sct.chains(o_loc, h2_loc)
@@ -14,7 +14,9 @@ h2o = sct.chains(o_loc, h2_loc)
 name = sa.getAtom(1)[0]
 mass = sa.getAtom(1)[1]
 
+data = sc.getInfo(2157)
+
 print(name)
 print(mass)
 
-na = sct.dataDisplay(sc.getInfo(2157), vp.color.white, vp.vector(3,3,3))
+na = sct.dataDisplay(data, vp.color.white, vp.vector(3,3,3))
