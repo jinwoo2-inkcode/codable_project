13e48779c1434c43b042ad4aa8eba9d122268fa5
jinwoo2-inkcode <60500457+jinwoo2-inkcode@users.noreply.github.com>
2021-10-10 15:52:31 -0500

    Update api_demo.py

--- codable/project/api_demo.py
+++ codable/project/api_demo.py
@@ -6,11 +6,32 @@ authentication = "vasp?API_KEY="+ API_KEY
 
 # request_type, identifier, parameters
 request_string = 'https://www.materialsproject.org/rest/v2/{}/{}/{}'
-temp = requests.get(request_string.format('materials','mp-1234', authentication))
-# print(request_string.format('materials','mp-1234', authentication))
 
+#Water Compound
+temp = requests.get(request_string.format('materials','mp-697111', authentication))
+# print(request_string.format('materials','mp-1234', authentication))
 
 #testing
 temp_dict = temp.json()
-pprint(temp_dict)
-#print(temp_dict['elements'])
+#pprint(temp_dict)
+# pprint(temp_dict['response'][0]['elements'])
+# pprint(temp_dict['response'][0]['energy'])
+# pprint(temp_dict['response'][0]['unit_cell_formula'])
+
+h_atom = temp_dict['response'][0]['unit_cell_formula']['H']
+o_atom = temp_dict['response'][0]['unit_cell_formula']['O']
+
+#Greatest common Denominator
+def gcd(a,b):
+    if(b==0):
+        return a
+    else:
+        return gcd(b,a%b)
+
+gcd_value = gcd(h_atom,o_atom)
+
+#print out water
+print('H' + str(int(h_atom/gcd_value)) + 'O')
+
+
+#pprint(temp_dict['response'][0]['volume'])
