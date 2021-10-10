acd5d5ac2e87f92d3da6682146ec50029d05a02b
jinwoo2-inkcode <60500457+jinwoo2-inkcode@users.noreply.github.com>
2021-10-10 17:27:36 -0500

    Update api_demo.py
    created simple user interface for testing

--- codable/project/api_demo.py
+++ codable/project/api_demo.py
@@ -7,21 +7,33 @@ authentication = "vasp?API_KEY="+ API_KEY
 # request_type, identifier, parameters
 request_string = 'https://www.materialsproject.org/rest/v2/{}/{}/{}'
 
+#user input compound name
+user_compound_input = input("Enter Compound")
+
 #Water Compound
-temp = requests.get(request_string.format('materials','mp-697111', authentication))
+temp = requests.get(request_string.format('materials',user_compound_input, authentication)) #Find how to format the compound input
+# temp = requests.get(request_string.format(F'materials','mp-1234', authentication))
 # print(request_string.format('materials','mp-1234', authentication))
 
-#testing
 temp_dict = temp.json()
-#pprint(temp_dict)
-# pprint(temp_dict['response'][0]['elements'])
-# pprint(temp_dict['response'][0]['energy'])
-# pprint(temp_dict['response'][0]['unit_cell_formula'])
 
-h_atom = temp_dict['response'][0]['unit_cell_formula']['H']
-o_atom = temp_dict['response'][0]['unit_cell_formula']['O']
+
+#components to display
+pprint(temp_dict['response'][0]['elements'])
+pprint(temp_dict['response'][0]['energy'])
+pprint(temp_dict['response'][0]['unit_cell_formula'])
+
+print(len(temp_dict['response'][0]['elements']))
+
+
+
+#testing with water
+# h_atom = temp_dict['response'][0]['unit_cell_formula']['H']
+# o_atom = temp_dict['response'][0]['unit_cell_formula']['O']
+
 
 #Greatest common Denominator
+"""
 def gcd(a,b):
     if(b==0):
         return a
@@ -30,8 +42,8 @@ def gcd(a,b):
 
 gcd_value = gcd(h_atom,o_atom)
 
-#print out water
+print out water
 print('H' + str(int(h_atom/gcd_value)) + 'O')
-
+"""
 
 #pprint(temp_dict['response'][0]['volume'])
